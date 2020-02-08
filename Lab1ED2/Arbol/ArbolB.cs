26a90f363576b9e3b832d57ed00735ecc1bf0c35
EdgarP43 <e.paredes-cas@outlook.com>
2020-02-08 09:08:06 -0600

    Insertar en raiz

--- Lab1ED2/Arbol/ArbolB.cs
+++ Lab1ED2/Arbol/ArbolB.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab1ED2.Arbol;
+using Lab1ED2.SodaViewModel;
 
 namespace Lab1ED2.Arbol
 {
     public class ArbolB
     {
+        private Nodo raiz;
+        public void inserta(SodaClass dato)
+        {
+            if (raiz == null)
+            {
+                Nodo nuevoNodo = new Nodo();
+                nuevoNodo.valorIzquierdo = dato;
+                raiz = nuevoNodo;
+
+            }
+            else
+            {
+                Nodo temporal = insertar(dato, raiz);
+                if (temporal != null)
+                {
+                    raiz = temporal;//si tuvo que partir la raiz significa que otro tuvo que subir y ese sera la nueva raiz
+                }
+
+            }
+           
+        }
+        public Nodo insertar(SodaClass dato, Nodo nodo)
+        {
+            return null;
+        }
     }
 }
